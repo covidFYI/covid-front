ddf88036c13680be85d3b232d1f658ed8bc64504
Manan <gouhari21@hotmail.com>
2020-04-05 22:45:14 +0530

    Added google analytics

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vueHeadful from 'vue-headful';
+import VueAnalytics from 'vue-analytics';
 
+
+Vue.use(VueAnalytics, {
+  id: 'UA-162786619-1',
+  router
+});
 Vue.component('vue-headful', vueHeadful);
 
 Vue.config.productionTip = false;
