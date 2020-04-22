99eb9fd2140bff97d6f1177de23fac771e92e5b7
Manan <gouhari21@hotmail.com>
2020-04-22 19:06:42 +0530

    fixed breadcrumb labels

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -42,7 +42,8 @@ export default {
     let faScript = document.createElement('script')
     faScript.setAttribute('src', 'https://kit.fontawesome.com/525c03e75b.js')
     document.head.appendChild(faScript)
-    if(!JSON.parse(localStorage.getItem('visited')) || this.timeLimitExpired()){
+    //if(!JSON.parse(localStorage.getItem('visited')) || this.timeLimitExpired()){
+    if(false){ //remove this before pushing.
       axios.get(`${this.URL}info_types/`)
       .then(resp => {
       
