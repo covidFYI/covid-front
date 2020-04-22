df2e60019bd78106fe294017e9d447f662f7fb74
Manan <gouhari21@hotmail.com>
2020-04-22 19:45:21 +0530

    Wrong branch, but added scroll to top button.

--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ import router from "./router";
 import vueHeadful from 'vue-headful';
 import VueAnalytics from 'vue-analytics';
 import vSelect from 'vue-select'
+//import GoTop from '@inotom/vue-go-top'
 
 Vue.use(VueAnalytics, {
   id: 'UA-162786619-1',
   router
 });
 
+//Vue.use(GoTop);
 Vue.component('vue-headful', vueHeadful);
 Vue.component('v-select', vSelect)
 
